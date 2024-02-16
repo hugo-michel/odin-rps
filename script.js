61455c6bd66f06f87b82540e7ff1554a7dcb92a9
hugo-michel <orayito62@gmail.com>
2024-02-16 22:40:06 +0100

    ajout ui pr rps

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let scoreComputer = 0;
 let scorePlayer = 0;
+const btnRock = document.querySelector("#btn-rock");
+const btnPaper = document.querySelector("#btn-paper");
+const btnScissors = document.querySelector("#btn-scissors");
+const result = document.querySelector("#result");
+const divScorePlayer = document.querySelector("#score-joueur");
+const divScoreOrdi = document.querySelector("#score-ordi");
+
+divScorePlayer.textContent = `score joueur = ${scorePlayer}`;
+divScoreOrdi.textContent = `score ordi = ${scoreComputer}`;
 
 function getComputerChoice() {
 	let randomNumber = Math.floor(Math.random() * 3);
@@ -12,57 +21,74 @@ function getComputerChoice() {
 	}
 }
 
-function getPlayerChoice() {
-	let choice = prompt("Quel est votre choix ?");
-	let playerChoice = choice.toUpperCase();
-	return playerChoice;
-}
-
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection == computerSelection) {
 		return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : match nul !`;
 	} else if (playerSelection == "ROCK") {
 		if (computerSelection == "PAPER") {
-            scoreComputer++;
-			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : ordi win !`;
+			scoreComputer++;
+			divScoreOrdi.textContent = `score ordi = ${scoreComputer}`;
+			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : ordi win la manche !`;
 		} else if (computerSelection == "SCISSORS") {
-            scorePlayer++;
-			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : player win !`;
+			scorePlayer++;
+			divScorePlayer.textContent = `score joueur = ${scorePlayer}`;
+			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : player win la manche !`;
 		}
 	} else if (playerSelection == "PAPER") {
 		if (computerSelection == "ROCK") {
-            scorePlayer++;
-			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : player win !`;
+			scorePlayer++;
+			divScorePlayer.textContent = `score joueur = ${scorePlayer}`;
+			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : player win la manche !`;
 		} else if (computerSelection == "SCISSORS") {
-            scoreComputer++;
-			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : ordi win !`;
+			scoreComputer++;
+			divScoreOrdi.textContent = `score ordi = ${scoreComputer}`;
+			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : ordi win la manche !`;
 		}
 	} else if (playerSelection == "SCISSORS") {
 		if (computerSelection == "PAPER") {
-            scorePlayer++;
-			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : player win !`;
+			scorePlayer++;
+			divScorePlayer.textContent = `score joueur = ${scorePlayer}`;
+			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : player win la manche !`;
 		} else if (computerSelection == "ROCK") {
-            scoreComputer++;
-			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : ordi win !`;
+			scoreComputer++;
+			divScoreOrdi.textContent = `score ordi = ${scoreComputer}`;
+			return `player a joué ${playerSelection} et ordi a joué ${computerSelection} : ordi win la manche !`;
 		}
 	}
 }
 
-function playGame() {
-	for (i = 0; i < 5; i++) {
-		let playerSelection = getPlayerChoice();
-		let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-	}
-    if (scorePlayer == scoreComputer) {
-        console.log(`match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : match nul !`);
-    }
-    else if (scorePlayer > scoreComputer) {
-        console.log(`match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : player win !`);     
-    }
-    else if (scorePlayer < scoreComputer) {
-        console.log(`match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : ordi win !`);     
-    }
-}
+btnRock.addEventListener("click", function() {
+	let computerSelection = getComputerChoice();
+	let playerSelection = "ROCK";
+	result.textContent = playRound(playerSelection, computerSelection);
+	if (scorePlayer === 5) {
+		result.textContent = `Match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : player win !`
+	};
+	if (scoreComputer === 5) {
+		result.textContent = `Match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : ordi win !`
+	};
+});
 
-playGame();
+btnPaper.addEventListener("click", function() {
+	let computerSelection = getComputerChoice();
+	let playerSelection = "PAPER";
+	result.textContent = playRound(playerSelection, computerSelection);
+	if (scorePlayer === 5) {
+		result.textContent = `Match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : player win !`
+	};
+	if (scoreComputer === 5) {
+		result.textContent = `Match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : ordi win !`
+	};
+});
+
+btnScissors.addEventListener("click", function() {
+	let computerSelection = getComputerChoice();
+	let playerSelection = "SCISSORS";
+	result.textContent = playRound(playerSelection, computerSelection);
+	if (scorePlayer === 5) {
+		result.textContent = `Match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : player win !`
+	};
+	if (scoreComputer === 5) {
+		result.textContent = `Match terminé ! le score final est player : ${scorePlayer} et ordi : ${scoreComputer} : ordi win !`
+	};
+});
